c3c271fb33e03883f90ffadfc311ebb263ea6ff6
Bahbi <superbahbi@gmail.com>
2016-05-11 23:22:22 -0700

    sender

--- modules/src/hello.py
+++ modules/src/hello.py
@@ -5,8 +5,8 @@ import config
 from templates.text import TextTemplate
 
 def process(input, entities, sender):
-    ACCESS_TOKEN = os.environ.get('FACEBOOK_ACCESS_TOKEN', config.ACCESS_TOKEN)
-    graph = facebook.GraphAPI(ACCESS_TOKEN)
+    FACEBOOK_ACCESS_TOKEN = os.environ.get('FACEBOOK_ACCESS_TOKEN', config.ACCESS_TOKEN)
+    graph = facebook.GraphAPI(FACEBOOK_ACCESS_TOKEN)
     profile = graph.get_object(str(sender))
     name = profile['name'].split()
     print "%s" % (sender)
