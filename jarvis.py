1d4eae4f0aba3145453d34949f561bc133c81f71
Bahbi <superbahbi@gmail.com>
2016-05-11 17:28:10 -0700

    change some var

--- jarvis.py
+++ jarvis.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def about():
-    return 'Just A Rather Very Intelligent System, now on Messenger!'
+    return 'Espurr, now on Messenger!'
 
 @app.route('/process/')
 def process():
--- modules/src/request.py
+++ modules/src/request.py
@@ -5,7 +5,7 @@ def process(input, entities=None):
   - Request a new feature, by including some sample queries and their expected results.\n
   - Report a bug (I couldn't handle the query and/or gave unexpected results), by including your search query and the expected result.'''
     template = ButtonTemplate(request)
-    template.add_web_url('Request / Report', 'https://github.com/swapagarwal/JARVIS-on-Messenger/issues/new')
+    template.add_web_url('Request / Report', 'http://bahbi.net')
     output = {
         'input': input,
         'output': template.get_message(),
