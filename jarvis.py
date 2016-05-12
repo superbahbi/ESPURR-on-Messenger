5ea553325e43d52e5312c878127fbfb7d0ab2511
Bahbi <superbahbi@gmail.com>
2016-05-11 22:06:50 -0700

    wit ai

--- jarvis.py
+++ jarvis.py
@@ -20,7 +20,7 @@ def process():
 
 @app.route('/search/')
 def search():
-    return json.dumps(modules.search(request.args.get('q')))
+    return json.dumps(modules.search(request.args.get('q')), None)
 
 @app.route('/webhook/', methods=['GET', 'POST'])
 def webhook():
