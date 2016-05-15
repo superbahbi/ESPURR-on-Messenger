0020173b08f9cf75999ab02dc5f2111cc302914a
Bahbi <superbahbi@gmail.com>
2016-05-15 16:08:25 -0700

    add sender to fact module

--- modules/src/fact.py
+++ modules/src/fact.py
@@ -1,7 +1,7 @@
 import requests
 from templates.text import TextTemplate
 
-def process(input, entities=None):
+def process(input, entities=None, sender):
     output = {}
     try:
         r = requests.get('http://numbersapi.com/random/trivia',headers={'Content-Type':'application/json'})	
