eb286a1c21a324ffd5ab2d70e783edd922a7956a
Bahbi <superbahbi@gmail.com>
2016-05-15 17:07:18 -0700

    weather fix

--- modules/src/weather.py
+++ modules/src/weather.py
@@ -3,9 +3,9 @@ import os
 import config
 from templates.text import TextTemplate
 
+OPENWEATHER_API = os.environ.get('OPENWEATHER_API', config.OPENWEATHER_API)
 def process(input, entities, sender):
     output = {}
-    OPENWEATHER_API = os.environ.get('OPENWEATHER_API', config.OPENWEATHER_API)
     try:
         location = entities['location'][0]['value']
         r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + OPENWEATHER_API)
