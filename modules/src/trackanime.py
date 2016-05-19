2b86fa298de6cc24908e15e6b2572c59f23ee8c0
superbahbi <superbahbi@gmail.com>
2016-05-19 16:26:01 -0700

    add firebase

--- modules/src/trackanime.py
+++ modules/src/trackanime.py
@@ -9,7 +9,8 @@ def process(input, entities, sender):
     output = {}
     try:
         r = firebase.FirebaseApplication(FIREBASE_URL, None)
-        result = r.get('/user', None)
+        result = firebase.post('/users', sender, {'print': 'silent'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+        //result = r.get('/user', None)
         template = TextTemplate()
         res = 'Name: %s' % (sender)
         output['input'] = input
