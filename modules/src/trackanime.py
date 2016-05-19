805698c7a902c48165a5f4a165ae5b72ea62eb64
superbahbi <superbahbi@gmail.com>
2016-05-19 14:53:19 -0700

    anime tracker

--- modules/src/trackanime.py
+++ modules/src/trackanime.py
@@ -7,13 +7,14 @@ from templates.text import TextTemplate
 FIREBASE_URL = os.environ.get('FIREBASE_URL', config.FIREBASE_URL)
 def process(input, entities, sender):
     output = {}
+    firebase = firebase.FirebaseApplication(FIREBASE_URL, None)
+    result = firebase.get('/user', None)
+    template = TextTemplate()
+    print firebase
+    print result
+    res = 'Name: %s' % (result)
     try:
-        firebase = firebase.FirebaseApplication(FIREBASE_URL, None)
-        result = firebase.get('/user', None)
-        template = TextTemplate()
-        print firebase
-        print result
-        res = 'Name: %s' % (result)
+        
         
         output['input'] = input
         output['output'] = TextTemplate(res).get_message()
