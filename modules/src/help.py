54b9053b08b41a188dc6d2b4926b45eb29280313
Bahbi <superbahbi@gmail.com>
2016-08-02 11:41:12 -0700

    get sender id

--- modules/src/help.py
+++ modules/src/help.py
@@ -8,9 +8,9 @@ def process(input, entities, sender):
     graph = facebook.GraphAPI(ACCESS_TOKEN)
     profile = graph.get_object("%s" % (sender))
     name = profile['first_name'].split()
-    help = '''Hi %s! I'm Espurr, your personal assistant.\nTell me things like the following:\n
+    help = '''Hi %s %s! I'm Espurr, your personal assistant.\nTell me things like the following:\n
   - define a superhero\n  - iron man 2 movie plot\n  - tell me a joke\n  - wiki html\n  - anything you want book\n  - random quote\n  - usd to eur rate\n
-I'm always learning, so do come back and say hi from time to time!\nHave a nice day.''' % (name[0])
+I'm always learning, so do come back and say hi from time to time!\nHave a nice day.''' % (name[0], sender)
     output = {
         'input': input,
         'output': TextTemplate(help).get_message(),
